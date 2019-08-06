e179a6ddb2031b8fc5e10ff7ff9f8e3ac7e705f5
MQXD <13899409337@163.comm>
2019-08-07 00:46:48 +0800

    合并代码

--- Shopping_Mall/src/main/java/com/xzy/dao/UsersMapper.java
+++
@@ -1,7 +0,0 @@
-package com.xzy.dao;
-
-import com.xzy.pojo.Goods;
-
-public interface UsersMapper {
-    public void addUser(Goods user);
-}
--- Shopping_Mall/src/main/java/com/xzy/service/base/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.xzy.service.base;
-
-import com.xzy.pojo.Goods;
-
-public interface UserService {
-    public void addUser(Goods u);
-}
